d155612d6aec5830621f85967c70fa2371472d69
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-21 00:15:37 +0000

    variable tutorial

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String tutString = "Hello World";
+    char tutChar = 'a';
+    int tutInt = 1;
+
+    // readonly modifier
+    final float tutFloat = 1.0f;
+    final boolean tutBool = false;
+
+
+    System.out.println(tutString);
+    System.out.println(tutChar);
+    System.out.println(tutInt);
+    System.out.println(tutFloat);
+    System.out.println(tutBool);
   }
 }
